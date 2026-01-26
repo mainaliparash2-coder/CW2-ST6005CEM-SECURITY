e52001a92919a0aec1e395175a2cecd3c8021336
parash0001 <mainaliparash@gmail.com>
2026-01-26 19:15:39 +0545

    OTP models to generate otp based on that and send those for authentication protection

---
+++ models/Otp.js
@@ -0,0 +1,22 @@
+// Libraries
+const mongoose = require('mongoose');
+
+const otpSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  otp: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 300  // Auto-delete after 5 minutes (300 seconds)
+  }
+});
+
+const OTP = mongoose.model('otps', otpSchema);
+
+module.exports = OTP;
