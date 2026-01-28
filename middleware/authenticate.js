5a82e865bad2056b1c52cc624040f0015bff1592
parash0001 <mainaliparash@gmail.com>
2026-01-28 21:15:41 +0545

    rate limiting feature and admin authentication

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,19 +1,33 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 const secretKey = process.env.SECRET_KEY;
 
-const authenticate = async function(req, res, next) {
+const authenticate = async function (req, res, next) {
   try {
-    const token = await req.cookies.AmazonClone;
-    // console.log(token);
-    const verifyToken = await jwt.verify(token, secretKey);
-    // console.log(verifyToken);
+    // const token = await req.cookies.AmazonClone;
+    // const verifyToken = await jwt.verify(token, secretKey);
+    // const rootUser = await User.findOne({ _id: verifyToken._id });
 
-    const rootUser = await User.findOne({ _id: verifyToken._id });
-    // console.log(rootUser);
+    // if (!rootUser) {
+    //   throw new Error("User not found");
+    // }
+
+    const token = req.cookies.AmazonClone;
+    if (!token) {
+      return res.status(401).json({ message: "No token provided" });
+    }
+
+    const verifyToken = jwt.verify(token, secretKey);
+
+    const rootUser = await User.findOne({
+      _id: verifyToken._id,
+      "tokens.token": token,
+    });
 
     if (!rootUser) {
-      throw new Error("User not found");
+      return res
+        .status(401)
+        .json({ message: "Token is invalid or logged out" });
     }
 
     req.token = token;
@@ -21,14 +35,13 @@ const authenticate = async function(req, res, next) {
     req.userId = rootUser._id;
 
     next();
-
   } catch (error) {
     res.status(400).json({
       status: false,
       message: "No token provided",
-      error: error
-    })
+      error: error,
+    });
   }
-} 
+};
 
 module.exports = authenticate;
---
+++ middleware/authenticateAdmin.js
@@ -0,0 +1,71 @@
+const jwt = require("jsonwebtoken");
+const Admin = require("../models/Admin");
+const adminSecretKey = process.env.ADMIN_SECRET_KEY || process.env.SECRET_KEY;
+
+const authenticateAdmin = async function (req, res, next) {
+  try {
+    // Check for token in cookie or Authorization header
+    let token = req.cookies.AdminToken;
+
+    if (!token && req.headers.authorization) {
+      token = req.headers.authorization.replace("Bearer ", "");
+    }
+
+    if (!token) {
+      throw new Error("No authentication token provided");
+    }
+
+    // Verify token
+    const verifyToken = jwt.verify(token, adminSecretKey);
+
+    // Check if it's an admin token
+    if (!verifyToken.isAdmin) {
+      throw new Error("Not authorized as admin");
+    }
+
+    // Find admin user
+    const adminUser = await Admin.findOne({
+      _id: verifyToken._id,
+      isActive: true,
+    });
+
+    if (!adminUser) {
+      throw new Error("Admin user not found or inactive");
+    }
+
+    // Attach admin info to request
+    req.token = token;
+    req.adminUser = adminUser;
+    req.adminId = adminUser._id;
+    req.adminRole = adminUser.role;
+
+    next();
+  } catch (error) {
+    res.status(401).json({
+      status: false,
+      message: "Authentication failed",
+      error: error.message,
+    });
+  }
+};
+
+// Middleware to check if admin is superadmin
+const requireSuperAdmin = async function (req, res, next) {
+  try {
+    if (req.adminRole !== "superadmin") {
+      return res.status(403).json({
+        status: false,
+        message: "This action requires superadmin privileges",
+      });
+    }
+    next();
+  } catch (error) {
+    res.status(403).json({
+      status: false,
+      message: "Authorization failed",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = { authenticateAdmin, requireSuperAdmin };
